38fcbe33c9395207138bd2078f73b579c192da69
Nicolas <nicolaspucci1989@gmail.com>
2022-02-07 15:50:51 -0300

    fix meses

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 
 const thisMonth = new Date().getMonth();
 const button = `
   <button class="btn btn-red">
-    <h5>30% de descuento para ${months[thisMonth]} y ${months[(thisMonth + 1) % months.length]}</h5>
+    <h5>30% de descuento para ${months[thisMonth + 1]} y ${months[(thisMonth + 2) % months.length]}</h5>
   </button>
 `
 const discount = document.getElementById('descuento');
