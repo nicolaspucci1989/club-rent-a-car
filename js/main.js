0d4406cbefef0037d4120c793d9a4994bd8a2d4b
Nicolas <nicolaspucci1989@gmail.com>
2022-02-07 13:53:21 -0300

    agregando descuento 30% off marzo y abril

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
-const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo',
-  'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 const thisMonth = new Date().getMonth();
 const button = `
   <button class="btn btn-red">
-    <h5>30% OFF</h5>
+    <h5>30% de descuento para ${months[thisMonth]} y ${months[(thisMonth + 1) % months.length]}</h5>
   </button>
 `
-
 const discount = document.getElementById('descuento');
-
 discount.innerHTML = button
-
