f09b9845e6d0df1ab6f776f652ab6820f67e98a5
nicolaspucci1989 <nicolaspucci1989@gmail.com>
2022-05-03 13:23:48 -0300

    use dynamic title (#1)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 const thisMonth = new Date().getMonth();
+const thisMonthString = months[thisMonth];
+const nextMonthString = months[(thisMonth + 1) % months.length];
+
 const button = `
   <button class="btn btn-red">
-    <h5>30% de descuento para ${months[thisMonth]} y ${months[(thisMonth + 1) % months.length]}</h5>
+    <h5>30% de descuento para ${thisMonthString} y ${nextMonthString}</h5>
   </button>
 `
 const discount = document.getElementById('descuento');
 discount.innerHTML = button
+
+
+const titleContent = `30% off ${thisMonthString} y ${nextMonthString} | Club Rent a Car - Alquiler de Autos en Bariloche, Villa La Angostura, San Martin de los andes`
+
+const title = document.querySelector('head > title').innerHTML = titleContent
