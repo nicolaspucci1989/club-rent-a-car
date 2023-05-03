bd380484ac709c49eb6f8ef94bf82ec0055ac4b0
Nicolas Pucci <nicolaspucci1989@gmail.com>
2023-05-03 13:58:01 +0000

    update months

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const nextMonthString = months[(thisMonth + 3) % months.length];
 
 const button = `
   <button class="btn btn-red">
-    <h5>30% de descuento para Marzo y Abril</h5>
+    <h5>30% de descuento para Junio y Julio</h5>
   </button>
 `
 const discount = document.getElementById('descuento');
